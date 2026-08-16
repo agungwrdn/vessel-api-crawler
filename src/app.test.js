17b1e1422e195f922ae48e1ab9c8c4d53d585c7e
rawkings <agungwar00@gmail.com>
2026-08-16 22:26:06 +0700

    refactor: change lancar job to start function and add tests for job entrypoints

---
+++ src/app.test.js
@@ -0,0 +1,11 @@
+const test = require('node:test')
+const assert = require('node:assert/strict')
+
+const { resolveJob } = require('./app')
+
+test('all selected jobs are callable entrypoints', () => {
+  const selectedJobs = resolveJob('all')
+
+  assert.ok(selectedJobs.length > 0)
+  selectedJobs.forEach((job) => assert.equal(typeof job, 'function'))
+})
--- src/jobs/index.js
+++ src/jobs/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   tracking: require('./vessel-tracking'),
-  lancar: require('./lancar-berkat-prima-gps'),
+  lancar: () => require('./lancar-berkat-prima-gps').start(),
   stock: require('./stock-broadcast'),
   ports: require('./port-generator'),
 };
